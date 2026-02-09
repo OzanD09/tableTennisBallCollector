afe2870345c9f83ffe77991570d7465bf69da03d
Ozan Dizdar <zndzdr@gmail.com>
2026-02-09 12:03:56 +0300

    added motor test file

--- kinematics/src/main.cpp
+++ kinematics/src/main.cpp
@@ -8,47 +8,28 @@ const int stepRight = 3;
 const int dirRight = 6;
 const int enablePin = 8;
 int stepPerRev = 200;
-int microStepping = 16;
-
-const double pi = 3.14159265358979323846;
-float wheelDia = 1.5; //diameter of wheel in cm
+int microStepping = 64;
 
 AccelStepper leftStepper(motorInterfaceType, stepLeft, dirLeft);
 AccelStepper rightStepper(motorInterfaceType, stepRight, dirRight);
 
-float calcRotations(float distance) {
-  float rotations = distance/(1.5*pi);
-  return rotations;
-}
-
-int moveForward(float distanceCM) {
-  float rotations = calcRotations(distanceCM);
-  long targetSteps = (long)(rotations * stepPerRev * microStepping);
-
-  leftStepper.move(targetSteps);
-  rightStepper.move(targetSteps);
-
-  while (leftStepper.distanceToGo() != 0 || rightStepper.distanceToGo() != 0){
-    leftStepper.run();
-    rightStepper.run();
-  }
-}
-
-
-// put function declarations here:
-int moveForward(int, int); //forwards movement in cm
-float calcRotations(float);
-
-
-
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(enablePin, OUTPUT);
+    digitalWrite(enablePin, LOW);
+
+    //leftStepper.setMaxSpeed(1000);
+    //leftStepper.setAcceleration(500);
+
+    //rightStepper.setMaxSpeed(1000);
+    //rightStepper.setAcceleration(500);
+
+    leftStepper.setSpeed(500);
+    rightStepper.setSpeed(500);
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    leftStepper.runSpeed();
+    rightStepper.runSpeed();
 }
-
-
-// put function definitions here:
